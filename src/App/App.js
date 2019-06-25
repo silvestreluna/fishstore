362e2eadf048075d21903f4a7800a49b821ed1b2
Silvestre Luna <slvtrluna@gmail.com>
2019-06-24 20:27:54 -0500

    added ligin

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import firebase from 'firebase/app';
 
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
 
 import './App.scss';
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const loadComponent = () => {
+      if (this.state.authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
     return (
       <div className="App">
-        <Auth />
-        <Home />
+        {loadComponent()}
       </div>
     );
   }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Auth.scss';
 
 class Auth extends React.Component {
-  render() {
-    return (
+ loginClickEvent = (e) => {
+   e.preventDefault();
+   const provider = new firebase.auth.GoogleAuthProvider();
+   firebase.auth().signInWithPopup(provider);
+ }
+
+ render() {
+   return (
       <div className="Auth">
         <h1>Auth</h1>
+        <button className="btn btn-secondary" onClick={this.loginClickEvent}>Login with Google</button>
       </div>
-    );
-  }
+   );
+ }
 }
 
 export default Auth;
