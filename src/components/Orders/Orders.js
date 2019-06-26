5ebfe4824b2b32822ed8d393245235789a169c08
Silvestre Luna <slvtrluna@gmail.com>
2019-06-25 21:15:29 -0500

    added axios call/getting data by uid

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import orderData from '../../helpers/data/orderData';
 
 import './Orders.scss';
 
 
 class Orders extends React.Component {
+  state = {
+    orders: [],
+  }
+
+  componentDidMount() {
+    orderData.getMyOrders(firebase.auth().currentUser.uid)
+      .then(orders => this.setState({ orders }))
+      .catch(err => console.error('No orders', err));
+  }
+
   render() {
     return (
       <div className="Orders">
---
+++ src/helpers/data/orderData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys.json';
+
+const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
+
+const getMyOrders = uid => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/orders.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((res) => {
+      const orders = [];
+      Object.keys(res.data).forEach((fbkey) => {
+        res.data[fbkey].id = fbkey;
+        orders.push(res.data[fbkey]);
+      });
+      resolve(orders);
+    })
+    .catch(err => reject(err));
+});
+
+export default { getMyOrders };
